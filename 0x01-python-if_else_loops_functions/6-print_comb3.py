ef59c070d1a840b220b18fbf7f2e612ae2631442
NAJIB-B <najibajibasu@gmail.com>
2024-01-16 12:28:55 +0100

    changed the f string to formats

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for j in range(1, 10):
         if i == 0 and j == 1:
-            print(f"{i:d}{j:d}", end="")
-        print(f", {i:d}{j:d}", end="")
+            print("{:d}{:d}".format(i, j), end="")
+        print(", {:d}{:d}".format(i, j), end="")
 print("")
