bca6ebd675746792d3e7e98d63ce1668afe58f7d
NAJIB-B <najibajibasu@gmail.com>
2024-01-16 12:47:40 +0100

    fixed task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range(1, 10):
+    for j in range(i + 1, 10):
         if i == 0 and j == 1:
             print("{:d}{:d}".format(i, j), end="")
-        print(", {:d}{:d}".format(i, j), end="")
+        else:
+            print(", {:d}{:d}".format(i, j), end="")
 print("")
