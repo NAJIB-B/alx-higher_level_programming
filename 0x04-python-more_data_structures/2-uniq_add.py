de2feda19fcf3cbf9e73137cc9bfcc79c96f220b
NAJIB-B <najibajibasu@gmail.com>
2024-01-31 08:35:03 +0100

    did tasks two

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    the_set = set(my_list)
+    uniq_list = list(the_set)
+    
+    total = 0
+
+    for i in uniq_list:
+        total += i
+    return total
