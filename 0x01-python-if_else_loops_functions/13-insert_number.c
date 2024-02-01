9d7d6bf518453393fb846fc48158f57bebc96004
NAJIB-B <najibajibasu@gmail.com>
2024-02-01 15:04:59 +0100

    Implemented function to insert a value in a sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,6 +16,9 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *prev, *current, *new_list, *temp;
+	
+	if (**head == NULL)
+		return (NULL);
 
 	prev = NULL;
 	current = *head;	
