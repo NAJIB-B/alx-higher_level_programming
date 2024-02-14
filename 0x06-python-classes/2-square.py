bb34d99c6a77aff7784b44da3ba92f2e46c243a0
NAJIB-B <najibajibasu@gmail.com>
2024-02-14 10:35:40 +0100

    did task two

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module defines a square class
+    with a private instance attribute
+    size
+    """
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise Value("size must be >= 0")
+        self.__size = size
