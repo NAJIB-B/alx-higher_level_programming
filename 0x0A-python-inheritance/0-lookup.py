9cb6c94873c70f313cedb032ecec90fb317498d6
NAJIB-B <najibajibasu@gmail.com>
2024-04-15 13:48:18 +0100

    did task 0 & 1 for inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This moddule describes a function that
+gets the attribute and method of an object"""
+
+
+def lookup(obj):
+    """Returns the attributes and methods
+    of an object"""
+
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains a class that inherits from
+the List class"""
+
+
+class MyList(list):
+    """inherits from the List class"""
+
+    def print_sorted(self):
+        """sorts a list"""
+        self.sort()
+        print(self)
