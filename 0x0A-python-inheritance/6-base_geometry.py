5239f573495be993d2820f9bc61944c972125108
NAJIB-B <najibajibasu@gmail.com>
2024-04-15 18:31:07 +0100

    did task six for inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometery class"""
+
+
+class BaseGeometry:
+    """Represent the base geometery"""
+    
+    def area(self):
+        """Defines an empty area function"""
+        raise Exception("area() is not implemented")
