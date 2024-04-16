4d0af174ab02b0bf68e03c07370cf1b6f30b65e5
NAJIB-B <najibajibasu@gmail.com>
2024-04-16 15:33:34 +0100

    did task 1 for input output in python

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a module that reads a text file"""
+
+
+def read_file(filename=""):
+    """Returns the input of a text file"""
+
+    with open(filename, encoding='utf-8') as f:
+        read_data = f.read()
+        print(read_data)
