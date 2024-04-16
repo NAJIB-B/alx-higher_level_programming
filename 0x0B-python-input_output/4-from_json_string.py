d331383d1a8608613383748b4ce7d4074841fa5a
NAJIB-B <najibajibasu@gmail.com>
2024-04-16 16:36:31 +0100

    did task 4 for input output in python

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a function that represents json in object
+format"""
+
+
+import json
+
+def from_json_string(my_str):
+    """Returns the object representation of a json string"""
+
+    return (json.loads(my_str))
