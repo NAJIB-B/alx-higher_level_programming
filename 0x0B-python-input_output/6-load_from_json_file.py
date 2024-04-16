daed3fb9a4a8a50eccdea3bbe9d82285f565195e
NAJIB-B <najibajibasu@gmail.com>
2024-04-16 16:59:43 +0100

    did task 6 for input output in python

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a function that creates an Object
+from a “JSON file”"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """This function creates an Object
+    from a “JSON file”"""
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
