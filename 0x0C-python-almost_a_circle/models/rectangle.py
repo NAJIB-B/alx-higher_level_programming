f0386c8ef17b9707320b01bc5e196d3f128068f7
NAJIB-B <najibajibasu@gmail.com>
2024-04-24 10:37:21 +0100

    fix indentation for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes an instance of a Rectangle
-
+      
         Args:
             width: compulsory argument
             height: compulsory argument
