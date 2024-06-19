8d9cc2b87053ef08bd4ad9161911a2a817ba591d
NAJIB-B <najibajibasu@gmail.com>
2024-06-19 23:31:11 +0100

    did task ten for javascript warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,9 +5,12 @@ const arg = parseInt(process.argv[2]);
 if (Number.isNaN(arg)) {
   console.log(1);
 } else {
-  let result = 1;
-  for (let i = arg; i > 0; --i) {
-    result *= i;
+  function factorial (arg) {
+    if (arg > 0) {
+      return (arg * factorial(arg - 1));
+	}
+    return 1;
   }
-  console.log(result);
+  console.log(factorial(arg));
 }
+
