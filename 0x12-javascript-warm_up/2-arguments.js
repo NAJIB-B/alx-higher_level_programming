b35a9b61cb6e34c818696044b5e80f145428fbc6
NAJIB-B <najibajibasu@gmail.com>
2024-06-19 21:53:34 +0100

    did task two for javascript warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 
 const argv = process.argv.slice(2);
 
-if (argv.length) {
-  console.log('Argument found');
-} else {
+if (argv.length === 0) {
   console.log('No argument');
+} else {
+  console.log('Argument found');
 }
