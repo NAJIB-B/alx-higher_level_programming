19cf1ec508b20fed4a1e6b2f1c8bd4279414985c
NAJIB-B <najibajibasu@gmail.com>
2024-06-20 22:11:49 +0100

    added the charprint method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,6 @@
 const SquareF = require('./5-square');
 
 class Square extends SquareF {
-
   charPrint (c) {
     for (let i = 0; i < this.height; ++i) {
       console.log(c ? c.repeat(this.width) : 'X'.repeat(this.width));
