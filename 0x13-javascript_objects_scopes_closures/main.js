76e0b524c30d250c049a2e6455a55b87ac7137f2
NAJIB-B <najibajibasu@gmail.com>
2024-06-20 21:44:47 +0100

    created the square class

--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
 
-const r1 = new Rectangle(2, 3);
-console.log('Normal:');
-r1.print();
+const Square = require('./5-square');
 
-console.log('Double:');
-r1.double();
-r1.print();
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
 
-console.log('Rotate:');
-r1.rotate();
-r1.print();
