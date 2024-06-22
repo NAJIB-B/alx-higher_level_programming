04fc9a4e0d293b3b7c4030e74b6abe50b848c07e
NAJIB-B <najibajibasu@gmail.com>
2024-06-22 14:02:14 +0100

    did tast 10 for sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,6 +1,6 @@
 -- showing all the record of second table
 -- in descending order
 
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
